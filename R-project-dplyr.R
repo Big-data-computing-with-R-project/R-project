a7d74ea52cdd4237a8662b46cb1b58e0886e734c
phonwalai123 <phonwalai123@gmail.com>
2020-09-17 20:55:49 +0700

    edit2

--- R-project-dplyr.R
+++ R-project-dplyr.R
@@ -9,6 +9,7 @@
 library(magrittr)
 library(lubridate)
 library(tidyverse)
+library(dplyr)
 library(gridExtra)
 library(ggforce)
 library(kableExtra)
@@ -18,6 +19,7 @@ library(leaflet)
 raw.data.full_data <- read.csv("~/R-project/full_data.csv")
 View(raw.data.full_data)
 dim(raw.data.full_data) # row 31978 column 10
+raw.data.full_data %>% ggplot(aes(total_cases))+geom_histogram()
 
 raw.data.full_data[1:10,1:10] %>%
   kable('latex',booktabs=T,caption='Raw Data (first 10 columns only)') %>%
@@ -55,16 +57,49 @@ data <- raw.data.full_data %>% select(-c(weekly_cases,weekly_deaths,biweekly_cas
 View(data)
 
 #Average of total_cases by location
-aggregate(total_cases~location,raw.data.full_data,mean)
+test_totalcase <- aggregate(total_cases~location,raw.data.full_data,mean)
+test_totalcase
+barplot(test_totalcase$total_cases,names.arg = test_totalcase$location)
+
+
+raw.data.full_data %>% group_by(date(date)) %>%
+  summarize(avg_totalcase = total_cases) %>%
+  ggplot(aes(avg_totalcase))+geom_histogram()
+
 
 raw.data.full_data %>%
   group_by(location) %>%
   summarize(mean(total_cases),mean(total_deaths))
 
+##ranking by total_cases top 10 country
+data <- raw.data.full_data %>% filter(date == max(date))%>%
+  select(location,date,new_cases,new_deaths,total_cases,total_deaths) %>%
+  mutate(ranking = dense_rank(desc(total_cases)))
+#view(data)
+n <- 9
+top_country <- data %>% filter(ranking <= n+1) %>%
+  arrange(ranking) %>% pull(location) %>% as.character()
+top_country %>% setdiff('World') %>% print()
+#view(top_country)
+
+data.latest <- data %>% filter(!is.na(location)) %>%
+  mutate(location=ifelse(ranking <= n+1,as.character(location),'0ther')) %>%
+  mutate(location = location %>% factor(levels=c(top_country),'0ther'))
+#view(data.latest)
+
+data.latest %<>% group_by(location) %>%
+  summarise(total_cases = sum(total_cases),new_cases = sum(new_cases), 
+            total_deaths = sum(total_deaths),new_deaths = sum(new_deaths))%>%
+  mutate(death.rate = (100*total_deaths/total_cases) %>% round(1))
+data.latest %<>% select(c(location,total_cases,new_cases,total_deaths,new_deaths ))
+view(data.latest)
+
+
+
 #ranking
-top <- 
-  raw.data.full_data %>%
-  #filter(date == as.Date("4/%d/%Y")) %in% c("Thailand","Angola") %>%
+top <- raw.data.full_data %>%
+  filter(date >= as.Date("2020-04-01") & date <= as.Date("2020-04-30")) 
+  #filter(date >= as.Date("4/%d/%Y")) %in% c("Thailand","Angola") %>%
   select(date:total_deaths)  %>%
   arrange(location,desc(total_deaths))
 view(top)
@@ -76,8 +111,9 @@ ranktop <-
   arrange(location,row_num)
 view(ranktop)
 
+
 hist(ranktop$total_deaths)
-  
+
 
 
 order(raw.data.full_data$date)
