80d591bce1e7baccbbc46a401ad1598d435b13c3
phonwalai123 <phonwalai123@gmail.com>
2020-09-17 11:36:24 +0700

    edit1

--- R-project-dplyr.R
+++ R-project-dplyr.R
@@ -1,49 +1,99 @@
-#install.packages("dbplyr")
-#install.packages("dplyr")
-#install.packages("RMySQL")
-#install.packages("DBI")
-#packageVersion("dplyr")
+#install.packages("magrittr") #pipe operation
+#install.packages("lubridate") #date operations
+#install.packages("tidyverse") #ggplot2,tidyr,dplyr
+#install.packages("gridExtra") #multiple grid-based plots on a page
+#install.packages("ggforce") #accelerating ggplot2
+#install.packages("kableExtra") #complex tables
+#install.packages("leaflet") #map
 
-library(dbplyr)
-library(dplyr)
-library(RMySQL)
-library(DBI)
+library(magrittr)
+library(lubridate)
+library(tidyverse)
+library(gridExtra)
+library(ggforce)
+library(kableExtra)
+library(leaflet)
 
-??src_mysql
-my_db <- src_mysql(
-  dbname = "full_data",
-  host = "localhost",
-  user = "root",
-  password = "1234"
-)
+##load data into R
+raw.data.full_data <- read.csv("~/R-project/full_data.csv")
+View(raw.data.full_data)
+dim(raw.data.full_data) # row 31978 column 10
 
-my_db
+raw.data.full_data[1:10,1:10] %>%
+  kable('latex',booktabs=T,caption='Raw Data (first 10 columns only)') %>%
+  kable_styling(font_size=5,latex_options = c('striped','hold_position','repeat_header'))
+View(raw.data.full_data)
 
-r <- tbl(my_db,
-         sql("select * from full_data where total_cases > 10000 "))
-r
+##get dates from column names
+rdate <- as.Date(raw.data.full_data$date,"%Y-%m-%d")
+range(rdate)
+##search data in column
+data <- raw.data.full_data %>% filter(date == "9/3/2020")
 
-View(r)
+min.date <- min(rdate)
+max.date <- max(rdate)
+min.date.txt <- min.date %>% format('%d %b %Y')
+max.date.txt <- max.date %>% format('%d %b %Y') %>% paste('UTC')
 
-r <- tbl(my_db,
-         sql("select date,location,total_cases,total_deaths from full_data
-             "))
-r
-barplot(r$total_cases,names.arg = r&total_deaths)
-d<-as.data.frame(r)
-barplot(d$total_cases,names.arg = r&total_deaths)
+##find missing values
+is.na(raw.data.full_data)
+##count missing values
+sum(is.na(raw.data.full_data))
+##Omit missing values
+na.omit(raw.data.full_data)
+##Remove missing values
+mean(raw.data.full_data$date,na.rm = TRUE)
 
-View(r)
+#type data
+str(raw.data.full_data)
+##convert from character to date
+data <- raw.data.full_data$date %>% as.Date(raw.data.full_data$date,"%Y-%m-%d")
+str(data)
+#View(data)
+#remove some columns
+data <- raw.data.full_data %>% select(-c(weekly_cases,weekly_deaths,biweekly_cases,biweekly_deaths))
+View(data)
 
-r <- tbl(my_db,
-         sql("select location,total_cases,count(*) as total_deaths,avg(total_cases) as avg_total_cases
-         from full_data group by location
-         order by avg_total_case desc
-             "))
-r
-barplot(r$avg_total_cases,names.arg = r&total_cases)
-d<-as.data.frame(r)
-barplot(d$avg_total_cases,names.arg = r&total_cases)
-View(r)
+#Average of total_cases by location
+aggregate(total_cases~location,raw.data.full_data,mean)
 
-rdate <- as.Date(full_data$date,"%m/%d/%y")
+raw.data.full_data %>%
+  group_by(location) %>%
+  summarize(mean(total_cases),mean(total_deaths))
+
+#ranking
+top <- 
+  raw.data.full_data %>%
+  #filter(date == as.Date("4/%d/%Y")) %in% c("Thailand","Angola") %>%
+  select(date:total_deaths)  %>%
+  arrange(location,desc(total_deaths))
+view(top)
+ranktop <- 
+  top %>%
+  group_by(location)%>%
+  mutate(row_num = row_number(desc(total_deaths))) %>%
+  filter(row_num == 1) %>%
+  arrange(location,row_num)
+view(ranktop)
+
+hist(ranktop$total_deaths)
+  
+
+
+order(raw.data.full_data$date)
+sorted <- raw.data.full_data[order(raw.data.full_data$date),]
+view(raw.data.full_data)
+
+cleanData <- function(data){
+  ##remove some columns
+  data %<>% select(-c(new_cases,new_deaths,total_cases,total_deaths))%>% rename(weekly_cases,weekly_deaths,biweekly_cases,biweekly_deaths)
+  ##convert from character to date
+  data %<>% gather(key=date,value = count, -location)
+  ##convert from character to date
+  data %<>% mutate(date = date %>% substr(2,8) %>% mdy())
+  ##aggregate by country
+  data %<>% group_by(location,date) %>% summarise(count,na.rm=T) %>% as.date.frame()
+  return(data)
+}
+
+data.fulldata <- raw.data.full_data %>% cleanData() %>% rename(fulldata=count)
