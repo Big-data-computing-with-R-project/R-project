bbb9ae46dc74e39a9e91df555a92c5f2bce5ddc3
Oeng <60010981@kmitl.ac.th>
2021-02-05 01:19:37 +0700

    show data table

--- UI.R
+++ UI.R
@@ -1,10 +1,127 @@
 library(shiny)
+library(DT)
 library(markdown)
+library(gridExtra)
+library(dplyr)
+library(lubridate)
+library(magrittr)
+library(ggplot2)
+library(tidyr)
+#library(knitr)
+library(normalr)
+library(ggcorrplot)
+library(flexdashboard)
+#library(MLRMPA)
+#??src_mysql
+my_db <- src_mysql(
+  dbname = "covid",
+  host = "localhost",
+  user = "root",
+  password = "1234"
+)
+my_db
 
-# Define UI ----
+##import data
+df_conf <- tbl(my_db, sql("select * from confirmed "))
+df_conf <- as.data.frame(df_conf)
+df_deaths <- tbl(my_db, sql("select * from deaths "))
+df_deaths <- as.data.frame(df_deaths)
+df_recover <- tbl(my_db, sql("select * from recovered "))
+df_recover <- as.data.frame(df_recover)
+##check the time frame of the data
+n.col <- ncol(df_conf)
+dates <- names(df_conf)[5:n.col]%>% mdy()
+range(dates)
+min.date <- min(dates)
+max.date <- max(dates)
+min.date.txt <- min.date %>% format('%d %b %Y')
+max.date.txt <- max.date %>% format('%d %b %Y')
+#clean data
+cleanData <- function(data) {
+  ## remove some columns
+  data %<>% select(-c(Province.State, Lat, Long)) %>% rename(country=Country.Region)
+  ## convert from wide to long format
+  data %<>% gather(key=date, value=count, -country)
+  ## convert from character to date
+  data %<>% mutate(date = date %>% mdy())
+  ## aggregate by country
+  data %<>% group_by(country, date) %>% summarise(count=sum(count, na.rm=T)) %>% as.data.frame()
+  return(data)
+}
+## clean the three data sets
+data.confirmed <- df_conf %>% cleanData() %>% rename(confirmed=count)
+data.deaths <- df_deaths %>% cleanData() %>% rename(deaths=count)
+data.recovered <- df_recover %>% cleanData() %>% rename(recovered=count)
+data <- data.confirmed %>% merge(data.deaths, all=T) %>% merge(data.recovered, all=T)
+## countries/regions with confirmed cases, excl. cruise ships
+countries <- data %>% pull(country) %>% setdiff('Cruise Ship')
+data.world <- data %>% group_by(date) %>%
+  summarise(country='World',
+            confirmed = sum(confirmed, na.rm=T),
+            deaths = sum(deaths, na.rm=T),
+            recovered = sum(recovered, na.rm=T))
+data %<>% rbind(data.world)
+data %<>% mutate(current.confirmed = confirmed - deaths - recovered)
+#rate
+data %<>% arrange(country, date)
+n <- nrow(data)
+day1 <- min(data$date)
+data %<>% mutate(new.confirmed = ifelse(date == day1, NA, confirmed - lag(confirmed, n=1)),
+                 new.deaths = ifelse(date == day1, NA, deaths - lag(deaths, n=1)),
+                 new.recovered = ifelse(date == day1, NA, recovered - lag(recovered, n=1)))
+data %<>% mutate(new.confirmed = ifelse(new.confirmed < 0, 0, new.confirmed),
+                 new.deaths = ifelse(new.deaths < 0, 0, new.deaths),
+                 new.recovered = ifelse(new.recovered < 0, 0, new.recovered))
+## death rate based on total deaths and recovered cases
+data %<>% mutate(rate.upper = (100 * deaths / (deaths + recovered)) %>% round(1))
+## lower bound: death rate based on total confirmed cases
+data %<>% mutate(rate.lower = (100 * deaths / confirmed) %>% round(1))
+## death rate based on the number of death/recovered on every single day
+data %<>% mutate(rate.daily = (100 * new.deaths / (new.deaths + new.recovered)) %>% round(1))
+data.long <- data %>%
+  select(c(country, date, confirmed, current.confirmed, recovered, deaths)) %>%
+  gather(key=type, value=count, -c(country, date))
+## set factor levels to show them in a desirable order
+data.long %<>% mutate(type=recode_factor(type, confirmed='Total Confirmed',
+                                         current.confirmed='Current Confirmed',
+                                         recovered='Recovered',
+                                         deaths='Deaths'))
+
+##Number of case World
+world <- filter(data.long,country == 'World')
+plot1 <- world %>% filter(type != 'Total Confirmed') %>%
+  ggplot(aes(x=date, y=count)) +
+  geom_area(aes(fill=type), alpha=0.5) +
+  labs(title=paste0('Numbers of Cases Worldwide - ', max.date.txt)) +
+  scale_fill_manual(values=c('red', 'green', 'black')) +
+  theme(legend.title=element_blank(), legend.position='bottom',
+        plot.title = element_text(size=7),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        legend.key.size=unit(0.2, 'cm'),
+        legend.text=element_text(size=6),
+        axis.text=element_text(size=7),
+        axis.text.x=element_text(angle=45, hjust=1))
+plot2 <- world %>%
+  ggplot(aes(x=date, y=count)) +
+  geom_line(aes(color=type)) +
+  labs(title=paste0('Numbers of Cases Worldwide (log scale) - ', max.date.txt)) +
+  scale_color_manual(values=c('purple', 'red', 'green', 'black')) +
+  theme(legend.title=element_blank(), legend.position='bottom',
+        plot.title = element_text(size=14),
+        axis.title.x=element_blank(),
+        axis.title.y=element_blank(),
+        legend.key.size=unit(0.2, 'cm'),
+        legend.text=element_text(size=14),
+        axis.text=element_text(size=14),
+        axis.text.x=element_text(angle=45, hjust=1)) +
+  scale_y_continuous(trans='log10')
+## show two plots side by side
+grid.arrange(plot1, plot2, ncol=2)
+
+# ------------------------ Define UI -------------------
 ui <- fluidPage(
-  dashboardHeader(title = "Covid-19"),
-  navbarPage("Navbar!",
+  navbarPage("COVID-19!",
              tabPanel("Over all"
              ),
              tabPanel("US",
@@ -12,13 +129,19 @@ ui <- fluidPage(
              ),
              tabPanel("Thailand",
                       
+             ),
+             tabPanel("DATA summary",
+                    DT::dataTableOutput("dataTable")
+                      
              )
   )
 )
 
 # Define server logic ----
 server <- function(input, output) {
- 
+  output$dataTable = DT::renderDataTable({
+    data
+    })
 }
 
 # Run the app ----
