b1f542ff2fed21ff642f49f3f8dcb8730c6eb8dd
Oeng <60010981@kmitl.ac.th>
2021-03-13 15:40:26 +0700

    add map

--- Dashboard-covid.R
+++ Dashboard-covid.R
@@ -1,5 +1,10 @@
 library(shiny)
 library(DT)
+library(fs)
+library(wbstats)
+library(leaflet)
+library(plotly)
+library(tidyverse)
 library(markdown)
 library(gridExtra)
 library(dplyr)
@@ -10,19 +15,53 @@ library(tidyr)
 library(normalr)
 library(ggcorrplot)
 library(flexdashboard)
-#library(MLRMPA)
-#??src_mysql
-my_db <- src_mysql(
-  dbname = "covid19",
-  host = "cvd-database.cmxpnyylhkiw.us-east-2.rds.amazonaws.com",
-  user = "admin",
-  password = "RprojectCovid19"
-)
-my_db
+library(shinydashboard)
+
 # ------------------------ Define UI -------------------
 ui <- fluidPage(
-  navbarPage("COVID-19!",
-             tabPanel("Over all"
+  title = "COVID-19 Global Cases",
+  #tags$head(
+  #  tags$link(rel = "shortcut icon", type = "image/png", href = "logo.png")
+  #),
+  tags$style(type = "text/css", ".container-fluid {padding-left: 0px; padding-right: 0px !important;}"),
+  tags$style(type = "text/css", ".navbar {margin-bottom: 0px;}"),
+  tags$style(type = "text/css", ".content {padding: 0px;}"),
+  tags$style(type = "text/css", ".row {margin-left: 0px; margin-right: 0px;}"),
+  tags$style(HTML(".col-sm-12 { padding: 5px; margin-bottom: -15px; }")),
+  tags$style(HTML(".col-sm-6 { padding: 5px; margin-bottom: -15px; }")),
+  navbarPage(title = div("COVID-19", style = "padding-left: 10px"),
+             inverse = TRUE,
+             collapsible = TRUE,
+             fluid       = TRUE,
+             tabPanel("Over all",
+                      fluidRow( 
+                        tags$head(
+                        tags$style(type = "text/css", "#overview_map {height: 48vh !important;}"),
+                        tags$style(type = 'text/css', ".slider-animate-button { font-size: 20pt !important; }"),
+                        tags$style(type = 'text/css', ".slider-animate-container { text-align: left !important; }"),
+                        tags$style(type = "text/css", "@media (max-width: 991px) { .details { display: flex; flex-direction: column; } }"),
+                        tags$style(type = "text/css", "@media (max-width: 991px) { .details .map { order: 1; width: 100%; } }"),
+                        tags$style(type = "text/css", "@media (max-width: 991px) { .details .summary { order: 3; width: 100%; } }"),
+                        tags$style(type = "text/css", "@media (max-width: 991px) { .details .slider { order: 2; width: 100%; } }")
+                      ),
+                        column(12,style='padding:10px;',
+                               fluidRow(
+                                 column(4,
+                                        valueBoxOutput("confirmBox")),
+                                 column(4,
+                                        valueBoxOutput("recoverBox")),
+                                 column(4,
+                                        valueBoxOutput("deathBox")),
+                               ),
+                               fluidRow(
+                                 column(8,
+                                        leafletOutput("plot1")
+                                 ),
+                                 column(4,"Fluid 6"
+                                 )
+                               )
+                        )
+                      )
              ),
              tabPanel("US",
                       
@@ -31,7 +70,9 @@ ui <- fluidPage(
                       
              ),
              tabPanel("DATA summary",
-                      DT::dataTableOutput("dataTable")
+                      column(12,style='padding:20px;',
+                             DT::dataTableOutput("dataTable")
+                      )
                       
              )
   )
@@ -39,6 +80,38 @@ ui <- fluidPage(
 
 # Define server logic ----
 server <- function(input, output) {
+  output$dataTable = DT::renderDataTable({
+    data.latest.all
+  })
+  output$plot1 <- renderLeaflet({
+    map.confirmed 
+  })
+  output$confirmBox <- renderValueBox({
+    valueBox(
+      mostconfirm$confirmed,
+      subtitle = "Confirmed",
+      icon     = icon("file-medical"),
+      color    = "blue",
+    )
+  })
+  output$recoverBox <- renderValueBox({
+    valueBox(
+      mostrecover$recovered,
+      subtitle = "Recover",
+      icon     = icon("file-medical"),
+      color    = "yellow",
+      width    = NULL
+    )
+  })
+  output$deathBox <- renderValueBox({
+    valueBox(
+      mostdeath$deaths,
+      subtitle = "Death",
+      icon     = icon("file-medical"),
+      color    = "yellow",
+      width    = NULL
+    )
+  })
   
 }
 
